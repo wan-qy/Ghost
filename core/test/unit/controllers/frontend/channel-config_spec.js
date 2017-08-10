c3aad7daa3fa49d5553c92f64ba53c16125d7195
wan-qy <wan-qy@users.noreply.github.com>
2017-08-10 19:54:46 +0800

    Revert "🦄  Channels: make RSS & Pagination configurable (#8857)"
    This reverts commit 1cc4be801024acdaf121482fa13419de51e6d100.

--- core/test/unit/controllers/frontend/channel-config_spec.js
+++ core/test/unit/controllers/frontend/channel-config_spec.js
@@ -1,22 +1,22 @@
 /*jshint expr:true*/
 var should = require('should'),
-    channelConfig = require('../../../../server/controllers/frontend/channel-config');
+    channelConfig = require('../../../../server/controllers/frontend/channel-config').get;
 
 describe('Channel Config', function () {
     it('should get the index config', function () {
-        var result = channelConfig.get('index');
+        var result = channelConfig('index');
         should.exist(result);
         result.name.should.eql('index');
     });
 
     it('should get the author config', function () {
-        var result = channelConfig.get('author');
+        var result = channelConfig('author');
         should.exist(result);
         result.name.should.eql('author');
     });
 
     it('should get the tag config', function () {
-        var result = channelConfig.get('tag');
+        var result = channelConfig('tag');
         should.exist(result);
         result.name.should.eql('tag');
     });
